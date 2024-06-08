166097f1b5463c4a3a6cac1f7ffd71e588a990f7
RohitXopBolta <rohitpapop766@gmail.com>
2024-06-08 16:10:15 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,185 @@
+
+# file name: [modca-Tele.py] (py - 3.9)
+
+from colorama import Fore, Back, Style
+import os,sys
+try:
+	import requests
+except:
+	os.system('pip3 install requests')
+import requests
+import threading
+from threading import active_count
+import urllib
+import time
+import os
+import sys
+import webbrowser
+import pyfiglet
+from time import sleep
+import platform
+
+webbrowser.open('https://t.me/hackingareas')       
+Z =  '\033[1;31m' 
+F = '\033[2;32m' 
+B = '\033[2;36m'
+X = '\033[1;33m' 
+C = '\033[2;35m'
+
+try:
+ from colorama import Fore
+ import colorama
+except ModuleNotFoundError:
+ os.system('pip install requests')
+ os.system('pip install pyfiglet')
+ os.system('clear')
+
+def say_stuff(stuff_to_say):
+    engine = pyttsx3.init()
+    engine.say(str(stuff_to_say))
+    engine.runAndWait()
+print(Fore.GREEN + Back.BLACK)
+
+
+print(Back.GREEN + Fore.BLACK + '')
+print(Back.BLACK +Fore.GREEN + '  ')
+
+
+print(Fore.CYAN + '           ✘ JOIN : @HACKINGAREAS                            ')
+print(Fore.GREEN + '          ')
+print(Fore.BLUE + '✘ MAKER ⇾ ' + Fore.RED + '@HACKINGAREAS')
+print(Fore.BLUE + '✘ Telegram ⇾' + Fore.RED + 'https://t.me/HACKINGAREAS')
+print(Fore.BLUE + '✘ My TG Acc⇾' + Fore.RED + 'HACKINGAREAS')
+print('✘ Your OS ⇾'+ Fore.BLUE + str(platform.system())+Fore.GREEN)
+def Modca(text, delay, add_new_line=True):
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+    if add_new_line:
+        print("\n", end="", flush=True)
+k = ("                                    𝗠𝗼𝗱𝗰𝗮 𓏺 𝗧𝗵𝗲 𝗟𝗼𝘀𝘁 🇩🇿⃤ . ( Legendary )" )
+print(C + k)
+print('      ----------------------------\<>/-------------------------')
+link1=input('       EnTeR YoUr LiNk ➠➠ ')
+
+n_threads = 50
+threads = []
+################
+def view2(proxy):
+    links = [link1]
+    for i in links:
+        channel = i.split('/')[3]
+        msgid = i.split('/')[4]
+        send_seen(channel, msgid, proxy)
+###############
+def send_seen(channel, msgid, proxy):
+    s = requests.Session()
+    proxies = {
+        'http': proxy,
+        'https': proxy,
+    }
+    try:
+        a = s.get("https://t.me/"+channel+"/"+msgid,
+                  timeout=10, proxies=proxies)
+        cookie = a.headers['set-cookie'].split(';')[0]
+    except Exception as e:
+        return False
+    h1 = {"Accept": "*/*", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9,fa;q=0.8,de;q=0.7", "Connection": "keep-alive", "Content-Length": "5", "Content-type": "application/x-www-form-urlencoded",
+          "Cookie": cookie, "Host": "t.me", "Origin": "https://t.me", "Referer": "https://t.me/"+channel+"/"+msgid+"?embed=1", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "User-Agent": "Chrome"}
+    d1 = {"_rl": "1"}
+    try:
+        r = s.post('https://t.me/'+channel+'/'+msgid+'?embed=1',
+                   json=d1, headers=h1, proxies=proxies)
+        key = r.text.split('data-view="')[1].split('"')[0]
+        now_view = r.text.split('<span class="tgme_widget_message_views">')[1].split('</span>')[0]
+        if now_view.find("K") != -1:
+            now_view = now_view.replace("K","00").replace(".", "")
+    except Exception as e:
+        return False
+    h2 = {"Accept": "*/*", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9,fa;q=0.8,de;q=0.7", "Connection": "keep-alive", "Cookie": cookie, "Host": "t.me",
+          "Referer": "https://t.me/"+channel+"/"+msgid+"?embed=1", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "User-Agent": "Chrome", "X-Requested-With": "XMLHttpRequest"}
+    try:
+        i = s.get('https://t.me/v/?views='+key, timeout=10,
+                  headers=h2, proxies=proxies)
+        if(i.text == "true"):
+            print('\033[2;35mmDone SeND ➠ \033[2;35m'+now_view)           
+    except Exception as e:
+        return False
+    try:
+        h3 = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9,fa;q=0.8,de;q=0.7",
+              "Cache-Control": "max-age=0", "Connection": "keep-alive", "Cookie": cookie, "Host": "t.me", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "none", "Sec-Fetch-User": "?1", "Upgrade-Insecure-Requests": "1", "User-Agent": "Chrome"}
+        s.get("https://t.me/"+channel+"/"+msgid, headers=h3,
+              timeout=10, proxies=proxies)
+    except Exception as e:
+        return False
+
+def scrap():
+    try:
+        https = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=https&timeout=0", proxies=urllib.request.getproxies(), timeout=5).text
+        http = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=0", proxies=urllib.request.getproxies(), timeout=5).text
+        socks = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=0", proxies=urllib.request.getproxies(), timeout=5).text
+    except Exception as e:
+        print(e)
+        return False
+    f = open("proxies.txt", "w")
+    f.write(https+"\n"+http)
+    f.close()
+    f = open("V.txt", "w")
+    f.write(socks)
+    f.close()
+    
+def checker(proxy):
+    proxies = {
+        'http': proxy,
+        'https': proxy,
+    }
+    try:
+
+        view2(proxy)
+    except Exception as e:
+        return False
+
+def start():
+    s = scrap()
+    if s == False:
+        return
+    list = open('proxies.txt', 'r')
+    proxies = list.readlines()
+    list.close()
+    for i in proxies:
+        p = i.split('\n')[0]
+        if not p:
+            continue
+        while active_count() > n_threads:
+            liiii=9
+        thread = threading.Thread(target=checker, args=(p,))
+        threads.append(thread)
+        thread.start()
+
+    list = open('V.txt', 'r')
+    proxies = list.readlines()
+    list.close()
+    for i in proxies:
+        p = i.split('\n')[0]
+        if not p:
+            continue
+        while active_count() > n_threads:
+          lii7ii=99
+        pr = "socks5://"+p
+        thread = threading.Thread(target=checker, args=(pr,))
+        threads.append(thread)
+        thread.start()
+    
+    
+    return True
+    
+            
+def process(run_for_ever:bool = False):
+    if run_for_ever:
+        while True:
+            start()
+    else:
+        start()
+        
+webbrowser.open('https://t.me/hackingareas')
+process(True)
